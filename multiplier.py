59be0b64b57cb41c440df1d915bad9462c1366e5
Isidore DelPierro <fontadore@gmail.com>
2021-07-17 02:44:20 +0100

    Basic instructions for this matrix BODMAS solution

---
+++ multiplier.py
@@ -0,0 +1,7 @@
+'''
+    This file will collect input from the user
+    and use it to determine the file
+    that holds the algorithm
+    for solving this particular */
+'''
+print("Please enter the dimensions of the matrices")
